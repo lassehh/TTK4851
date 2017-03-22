04301d0c4bbb8fa52cef7b6bc03d76bab2c37b20
lassehh <lassehh@gmail.com>
2017-03-22 12:16:15 +0100

    rearranged the module and update domain init

--- src/path_finding/maintesting.py
+++ src/path_finding/maintesting.py
@@ -1,80 +1,41 @@
 from PIL import Image
 import numpy as np
-import GenericSearchNode
-import GenericAStar
+#import GenericSearchNode
+from PathFinder import *
+from Domain import *
+
+from time import time
 import os
 import webbrowser
 
 
-# import matplotlib.image as img
-# image = img.imread(file_name)
-# print image.shape
-
-
 def main():
-    # binaryCamImg = np.load('binary_matrix.npy')
-    # print binaryCamImg.shape
-    # img = Image.new('1', binaryCamImg.shape)
-    # pixels = img.load()
-    #
-    # for i in range(img.size[0]):
-    #     for j in range(img.size[1]):
-    #         pixelValue = binaryCamImg[i][j]
-    #         pixels[i,j] = int(pixelValue)
-    # img.save('robot_bw_view.png')
+
+    pixels = np.load("binary_matrix.npy")
+    map = Map(pixels, [0,0], [400,400])
+    #map.printMap()
+    #start = time()
+    print map.map[50,40].g
+    pathSpace = AStar(map, "BestFS")
+
+    start = time()
+    pathSpace.best_first_search()
+    print time() - start
+
+    #binCameraImage = np.load('binary_matrix.npy')
+    #print binCameraImage.shape
+    #pixels = Image.new('1', binCameraImage.shape)
+    #pixels = img.load()
+
+    # for i in range(pixels.size[0]):
+    #      for j in range(pixels.size[1]):
+    #          pixelValue = binCameraImage[i][j]
+    #          pixels[i,j] = int(pixelValue)
+    # print pixels[5,5]
+    # pixels.save('robot_bw_view.png')
     # webbrowser.open('robot_bw_view.png')
-
-    #data = np.fromfile('board-1-3.txt', None)
-    #print data
-    #txtfile = open('board-1-3.txt', 'r')
-    #with open('board-1-3.txt') as file:
-
-    Map = GenericAStar.Map('board-1-3.txt')
-    Map.printMap()
-    print Map.start
-    AStar = GenericAStar.AStar(Map, "BestFS")
-    AStar.best_first_search()
-    print 'fuck yes'
-
-
-    # Fix pixel.state: kan ikke vaare liste...
 
 
 
 main()
 
-
-
-
-
-    # os.system("pause")
-    # for x in xrange(0,903):
-    #     for y in xrange(0,971):
-    #         print numpyArray[x][y]
-    #img = Image.fromarray(numpyArray, '1')
-    #img.save("robot_view.jpeg")
-    #webbrowser.open('robot_view.jpeg')
-# def main():
-#     img =  Image.open('maze.jpg')
-#     img_gray = img.convert('L')
-#
-#     # Let numpy do the heavy lifting for converting pixels to pure black or white
-#     bw = np.asarray(img_gray).copy()
-#
-#     # Pixel range is 0...255, 256/2 = 128
-#     bw[bw < 128] = 0  # Black
-#     bw[bw >= 128] = 255  # White
-#     # print image.bits, image.size, image.format
-#
-#     imfile = Image.fromarray(bw)
-#     imfile.save("result_bw.png")
-#     webbrowser.open('result_bw.png')
-#     print bw[3]
-#     # for x in xrange(img.size[0]):
-#     #     for y in xrange(img.size[1]):
-#     #         r, g, b =  img.getpixel((x,y))
-#     #         print (r,g,b)
-#     #         if g > 150 and r < 100 and b < 100:
-#     #             os.system("pause")
-#
-#     #os.system("pause")
